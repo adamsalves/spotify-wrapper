db899fbc70dadf31d7cc271d072eb470bc1e0b61
Adams Alves <alves.adams@gmail.com>
2021-09-01 16:55:26 -0300

    disable eslint

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 export const search = (query, type) =>
+  // eslint-disable-next-line no-undef
   fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`).then(data => data.json());
 
 export const searchArtists = () => {};
