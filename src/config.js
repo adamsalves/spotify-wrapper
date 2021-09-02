c4663e30792620897d215fbf0c731ead7e643934
Adams Alves <alves.adams@gmail.com>
2021-09-02 18:57:27 -0300

    Add auth flow

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
-const API_URL = 'https://api.spotify.com/v1';
+const TOKEN_API = 'BQDUNdDXo0NkauBkIszPn6Oov2STZ0oRhbBRYdqWZ9XW2mggJ63RrRbRpHLSmIHyjWXf6UGkvGuAr5GuIgYUvVUj24XxJY8--lv4VXSgofoqVH7r3bnLe6MngGn5w5E7TYWgcBZC4GEReaUh';
 
-export default API_URL;
+export const API_URL = 'https://api.spotify.com/v1';
 
+export const HEADERS = {
+  headers: {
+    Authorization: `'Bearer ${TOKEN_API}'`,
+  }
+}
