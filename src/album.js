e9b21fef851d285ae6982ede6b3a33c88f825655
Adams Alves <alves.adams@gmail.com>
2021-09-03 15:51:07 -0300

    fix lint

--- src/album.js
+++ src/album.js
@@ -1,7 +1,7 @@
-export default function album () {
+export default function album() {
   return {
-    getAlbum: (id) => this.request(`${this.apiURL}/albums/${id}`),
-    getAlbums: (ids) => this.request(`${this.apiURL}/albums/?ids=${ids}`),
-    getTracks: (id) => this.request(`${this.apiURL}/albums/${id}/tracks`),
+    getAlbum: id => this.request(`${this.apiURL}/albums/${id}`),
+    getAlbums: ids => this.request(`${this.apiURL}/albums/?ids=${ids}`),
+    getTracks: id => this.request(`${this.apiURL}/albums/${id}/tracks`),
   };
 }
